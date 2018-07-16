061b1d15aaf1dff9ff6227c9616295793a9341f2
CHPrado <christianprado.contato@gmail.com>
2018-07-16 15:01:44 -0300

    Commited

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=c:\workspace\py\locadora\locadora\venv"
+set "VIRTUAL_ENV=C:\workspace\py\locadora\PyQt4_locadora\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
